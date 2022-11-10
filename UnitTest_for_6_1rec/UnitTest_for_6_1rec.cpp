f1d7516acea329eba90158a3211a68f8337e01de
MarichkaPM <tanechnyk.marichka@gmail.com>
2022-11-10 23:02:01 +0200

    first commit

---
+++ UnitTest_for_6_1rec/UnitTest_for_6_1rec.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laboratorna 6_1_rec/Lab 6_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestfor61rec
+{
+	TEST_CLASS(UnitTestfor61rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t = 0;
+			int arr[8] = { -1, 0, 3, -2, -8, 4, 7, -2 };
+			int suma = 0;
+			t = Sum(arr, suma, 0, 8);
+			Assert::AreEqual(-13, t);
+		}
+	};
+}
